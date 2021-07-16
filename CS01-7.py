729067651fc8609c35f099e118b38540b9493919
jiratpanya <58562504+jiratpanya@users.noreply.github.com>
2021-07-16 10:04:24 +0700

    aroi

---
+++ CS01-7.py
@@ -0,0 +1,23 @@
+A=int(input("enter number:"))
+if(A <= 10):
+    print("ไม่ผ่าน")
+elif(A <= 20):
+    print("ปรับปรุง")
+else :
+    print("ผ่าน")
+
+    B=int(input("enter number:"))
+if(B <= 10):
+    print("ไม่ผ่าน")
+elif(B <= 20):
+    print("ปรับปรุง")
+else :
+    print("ผ่าน")
+
+    C=int(input("enter number:"))
+if(C <= 10):
+    print("ไม่ผ่าน")
+elif(C <= 20):
+    print("ปรับปรุง")
+else :
+    print("ผ่าน")
